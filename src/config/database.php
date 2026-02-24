8347d1d264dfa592dcdbb56c232b85e5f85a853b
it-Husillos <ihusillos@termosalud.com>
2026-02-24 13:47:06 +0100

    preueba redis

--- src/config/database.php
+++ src/config/database.php
@@ -146,7 +146,7 @@ return [
         'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'predis'),
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
