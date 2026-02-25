68cc995921bba947aac8fa24e67cba26dc5f4d9c
it-Husillos <ihusillos@termosalud.com>
2026-02-25 10:09:22 +0100

    comprobación de formulario de autenticación

--- src/resources/views/layouts/auth.blade.php
+++ src/resources/views/layouts/auth.blade.php
@@ -3,9 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate">
-    <meta http-equiv="Pragma" content="no-cache">
-    <meta http-equiv="Expires" content="0">
     <title>@yield('title', 'Auth')</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -21,6 +18,5 @@
         </div>
     </div>
 
-    @include('layouts.reloadScript')
 </body>
 </html>
